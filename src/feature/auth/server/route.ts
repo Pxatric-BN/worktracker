516b9d59c67d4ea4bb82daea2b604ed2ef34e5df
Chayanon <chayanon.ba@vulcan.bluebik.com>
2026-02-25 14:14:33 +0700

    Feat: session middleware

--- src/feature/auth/server/route.ts
+++ src/feature/auth/server/route.ts
@@ -6,9 +6,17 @@ import { createAdminClient } from "@/lib/appwrite";
 import { ID } from "node-appwrite";
 import { setCookie,deleteCookie } from "hono/cookie"
 import { AUTH_COOKIE } from "../constants";
+import { sessionMiddleware } from "@/lib/session-midleware";
 
 
 const app = new Hono()
+    .get("/current", 
+        sessionMiddleware, 
+        (c)=>{
+            const user= c.get("user")
+            return c.json({data:user})
+        }
+    )
     .post(
         "/login",
         zValidator("json", useSignInSchema), 
@@ -61,10 +69,15 @@ const app = new Hono()
             return c.json({ success: true})
         }   
     )
-    .post("/logout", (c) => {
+    .post("/logout", sessionMiddleware, async (c) => {
+
+        const account = c.get("account") 
+
         deleteCookie(c, AUTH_COOKIE);
+        await account.deleteSession("current")
 
         return c.json({ success:true})
-    })
+        }
+    )
 
 export default app;
