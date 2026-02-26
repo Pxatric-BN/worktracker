eaa98c51c4fcf29324b9f7db3f25e259d5baa70d
Chayanon <chayanon.ba@vulcan.bluebik.com>
2026-02-26 18:01:25 +0700

    Feat : upload image workspace

--- src/feature/workspaces/components/createWorkspaceForm.tsx
+++ src/feature/workspaces/components/createWorkspaceForm.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useRef } from "react";
 import { createWorkspacesSchema } from "../hooks/useworkspaceSchema";
+import Image from "next/image";
+import type { ChangeEvent } from "react";
 import { z } from "zod";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
@@ -16,6 +19,8 @@ import { Separator } from "@/components/ui/separator";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useCreateWorkspaces } from "../hooks/useCreateWorkspaces";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { ImageIcon } from "lucide-react";
 
 interface CreateWorkspaceFormProps {
   onCancel?: () => void;
@@ -26,15 +31,37 @@ type CreateWorkspaceFormValues = z.infer<typeof createWorkspacesSchema>;
 export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
   const { mutate, isPending } = useCreateWorkspaces();
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const form = useForm<CreateWorkspaceFormValues>({
     resolver: zodResolver(createWorkspacesSchema),
     defaultValues: {
       name: "",
+      image: undefined,
     },
   });
 
   const onSubmit = (values: CreateWorkspaceFormValues) => {
-    mutate({ json: values });
+    const finalValues = {
+      ...values,
+      image: values.image instanceof File ? values.image : "",
+    };
+    mutate(
+      { form: finalValues },
+      {
+        onSuccess: () => {
+          form.reset();
+          // TODO: Redirect to new workspace
+        },
+      },
+    );
+  };
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      form.setValue("image", file);
+    }
   };
 
   return (
@@ -62,6 +89,60 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
                     </FormControl>
                     <FormMessage />
                   </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="image"
+                render={({ field }) => (
+                  <div className=" flex flex-col gap-y-2">
+                    <div className="flex items-center gap-x-5">
+                      {field.value ? (
+                        <div className="size-[72px] relative rounded-md overflow-hidden">
+                          <Image
+                            alt="workspaceImage"
+                            fill
+                            className="object-cover"
+                            src={
+                              field.value instanceof File
+                                ? URL.createObjectURL(field.value)
+                                : field.value
+                            }
+                          />
+                        </div>
+                      ) : (
+                        <Avatar className="size-[72px]">
+                          <AvatarFallback>
+                            <ImageIcon className="size-[36px] text-neutral-400" />
+                          </AvatarFallback>
+                        </Avatar>
+                      )}
+                      <div className="flex flex-col">
+                        <p className="text-sm">Workspace Icon</p>
+                        <p className="text-sm text-muted-foreground">
+                          JPG,PNG, SVG or JPEG max 1mb
+                        </p>
+                        <input
+                          className="hidden"
+                          type="file"
+                          accept=".jpeg, .png, .svg, .jpg"
+                          ref={inputRef}
+                          onChange={handleImageChange}
+                          disabled={isPending}
+                        />
+                        <Button
+                          type="button"
+                          disabled={isPending}
+                          variant="outline"
+                          size="sm"
+                          className="w-fit mt-2"
+                          onClick={() => inputRef.current?.click()}
+                        >
+                          Upload Image
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
                 )}
               />
             </div>
--- src/feature/workspaces/server/route.ts
+++ src/feature/workspaces/server/route.ts
@@ -2,18 +2,27 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { createWorkspacesSchema } from "../hooks/useworkspaceSchema";
 import { sessionMiddleware } from "@/lib/session-midleware";
-import { DATABASE_ID, WORKSPACE_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACE_ID } from "@/config";
 import { ID } from "node-appwrite";
 
 const app = new Hono().post(
   "/",
-  zValidator("json", createWorkspacesSchema),
+  zValidator("form", createWorkspacesSchema),
   sessionMiddleware,
   async (c) => {
     const databases = c.get("databases");
+    const storage = c.get("storage");
     const user = c.get("user");
-    const { name } = c.req.valid("json");
-
+    const { name, image } = c.req.valid("form");
+    let imageFileId: string | undefined;
+    if (image instanceof File) {
+      const file = await storage.createFile(
+        IMAGES_BUCKET_ID,
+        ID.unique(),
+        image,
+      );
+      imageFileId = file.$id;
+    }
     const workspaces = await databases.createDocument(
       DATABASE_ID,
       WORKSPACE_ID,
@@ -21,6 +30,7 @@ const app = new Hono().post(
       {
         name,
         userId: user.$id,
+        imageUrl: imageFileId,
       },
     );
     return c.json({ data: workspaces });
