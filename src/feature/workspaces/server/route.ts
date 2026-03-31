4fd75eeb066307342bb009cbdbdfe23282e8556c
Chayanon <chayanon.ba@vulcan.bluebik.com>
2026-03-31 14:24:30 +0700

    Feat: workspaces members

--- src/feature/workspaces/server/route.ts
+++ src/feature/workspaces/server/route.ts
@@ -2,15 +2,34 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { createWorkspacesSchema } from "../hooks/useworkspaceSchema";
 import { sessionMiddleware } from "@/lib/session-midleware";
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACE_ID } from "@/config";
-import { ID } from "node-appwrite";
+import {
+  DATABASE_ID,
+  IMAGES_BUCKET_ID,
+  MEMBER_ID,
+  WORKSPACE_ID,
+} from "@/config";
+import { ID, Query } from "node-appwrite";
+import { MemberRole } from "@/feature/members/types";
+import { genereteInviteCode } from "@/lib/utils";
 
 const app = new Hono()
   .get("/", sessionMiddleware, async (c) => {
+    const user = c.get("user");
     const databases = c.get("databases");
 
-    const workspaces = await databases.listDocuments(DATABASE_ID, WORKSPACE_ID);
-    return c.json({ data: workspaces });
+    const members = await databases.listDocuments(DATABASE_ID, MEMBER_ID, [
+      Query.equal("userId", user.$id),
+    ]);
+    if (members.total === 0) {
+      return c.json({ data: { documents: [] }, total: 0 });
+    }
+    const workspaceIds = members.documents.map((member) => member.workspaceId);
+    const workspaces = await databases.listDocuments(
+      DATABASE_ID,
+      WORKSPACE_ID,
+      [Query.orderDesc("$createdAt"), Query.contains("$id", workspaceIds)],
+    );
+    return c.json({ data: workspaces, total: workspaces.total });
   })
   .post(
     "/",
@@ -38,9 +57,16 @@ const app = new Hono()
           name,
           userId: user.$id,
           imageUrl: imageFileId,
+          inviteCode: genereteInviteCode(6),
         },
       );
+      await databases.createDocument(DATABASE_ID, MEMBER_ID, ID.unique(), {
+        userId: user.$id,
+        workspaceId: workspaces.$id,
+        role: MemberRole.ADMIN,
+      });
       return c.json({ data: workspaces });
     },
   );
+
 export default app;
